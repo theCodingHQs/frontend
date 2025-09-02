3ed6aa34233876d8c2526336b251efc4a91d5634
theCodingHQs <mrranjeet11115@gmail.com>
2025-09-02 20:33:18 +0530

    basic practice

---
+++ 29-08/javascript.js
@@ -0,0 +1,11 @@
+let num1 = "1234";
+let num2 = 12345;
+// true or false
+let isEven = 12345 % 2 == 0;
+let newVar = null;
+let sum = num2 + num1;
+
+let data = [true, null, undefined, "123", 123];
+let user = { name: "Aakash", age: 13 };
+
+console.log(data);
