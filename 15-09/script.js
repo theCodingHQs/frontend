40a6d07528888070cfccb8b7f13d52533ecff9cf
theCodingHQs <mrranjeet11115@gmail.com>
2025-09-15 20:47:10 +0530

    array methods

---
+++ 15-09/script.js
@@ -0,0 +1,17 @@
+const arr = ["name", "age", "location"];
+
+"name", 0, arr;
+const user = { name: "Aakash", age: 23, location: "pune" };
+
+const users = [
+  { name: "Aakash", age: 23, location: "pune" },
+  { name: "Amar", age: 15, location: "pune" },
+  { name: "Rahul", age: 25, location: "pune" },
+  { name: "Neha", age: 14, location: "pune" },
+];
+
+const xyz = function (a, b, c) {
+  console.log(a, b, c);
+};
+
+arr.map(xyz);
